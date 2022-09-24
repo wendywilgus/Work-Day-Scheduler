42d4c35c763206176ee30777821b04a5673f1768
Wendy Wilgus <96785281+wendywilgus@users.noreply.github.com>
2022-09-24 18:35:48 -0400

    "correcting schedule issues"

--- Develop/script.js
+++ Develop/script.js
@@ -1,45 +1,56 @@
-var currentDate = moment().format("MMM Do, YYYY");
-var hour = moment().format('h'); 
-var time = document.querySelector('time');
-var timeText = "";
+var now = moment(Date.now);
+var hour = moment(Date.now()).startOf('day');
+console.log("startOfDay", hour.format('h a'));
+console.log(now);
 
-// function to call date from Moment
-function displayDate()  {
-    var currentDate = moment();$("#currentDate").text(currentDate.format("MMM Do, YYYY"));
-    var displayMoment = document.getElementById('output')
+var todaysDate = moment().format("MMMM Do, YYYY");
+console.log("todaysDate", todaysDate);
 
-}
+var time = document.querySelector("time");
+var textarea = document.querySelector("textarea");
+var button = document.querySelector("button");
 
-// console.log(currentDate);
-displayDate();
+const dateElement = document.getElementById("currentDate");
+dateElement.innerHTML = `Today is ${todaysDate}`;
 
 var scheduleArray = [];
-$('time').each(function ()  {
+$('textarea').each(function ()  {
     var id = $(this).attr('id');
     scheduleArray.push(id);
 });
-// console.log(scheduleArray);
+console.log(scheduleArray);
+
+
+
+
+function init() {
+    window.localStorage.getElementById("textarea");
+    var time = moment($(e).attr("datetime"));
+if(now.diff(time, 'days') <= 1) {
+    // getting the relative output
+}
+console.log(time);
+var ago = now.from(time)
+console.log(ago);
+}
+
+
 
 function compareHour()  {
-    var hour = moment();$("#time").text
+    
+    // for ( i = 0; i < scheduleArray.length; i++)  {
+    //         scheduleArray[i].removeClass("past present future");
+    
+    if (hour > scheduleArray[i].data("hour")) {
+        scheduleArray[i].setAttribute(".past");
+    }
 }
-// }
-
-// $(".form-control").each(function () {
-//     var timeTest = parseInt($(this).attr("id"));
-//     hour = parseInt(hour);
-    // console.log(timeTest);
-    // console.log(hour);
-    //  console.log(this);
-    // if (hour > timeTest) {
-    //     $(this).addClass("past");
-    // } else if (hour < timeTest) {
-    //     $(this).addClass("future");
-    // } else {
-    //     $(this).addClass("present");
-    // }
-// })
 
 
-// console.log(time);
-// console.log(hour)
+//function to save user Events when page is refreshed
+function saveEvents(event)  {
+    event.preventDefault();
+    localStorage.setItem(textarea.textContent);
+}
+
+button.addEventListener("click", saveEvents);
